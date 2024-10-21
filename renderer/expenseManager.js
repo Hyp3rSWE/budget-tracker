aa5681e33a9f32d41c6f6ea79c856ef083095a33
nesrine-aibot <124919116+nesrine-aibot@users.noreply.github.com>
2024-10-21 17:28:31 +0200

    Update expenseManager.js

--- renderer/expenseManager.js
+++ renderer/expenseManager.js
@@ -1,19 +1,43 @@
 export class ExpenseManager {
     constructor() {
-        this.expenses = [
-            { name: 'Veg', price: 40.0 },
-            { name: 'Fruit', price: 70.0 },
-            { name: 'Fuel', price: 60.0 }
-        ];
+        database = "expenses-tracker";
     }
 
+    
     addExpense(expenseName, price) {
-        const newExpense = { name: expenseName, price: price };
-        this.expenses.push(newExpense);
+        const insertQuery = `INSERT INTO expenses (name, price) VALUES (?, ?)`;
+        this.database.run(insertQuery, [expenseName, price], function(err) {
+            if (err) {
+                console.error('Error adding expense:', err.message);
+            } else {
+                console.log(`Expense added with ID: ${this.lastID}`);
+            }
+        });
     }
 
-    getExpenses() {
-        return this.expenses;
+    getExpenses(callback) {
+        const selectQuery = `SELECT * FROM expenses`;
+        this.database.all(selectQuery, [], (err, rows) => {
+            if (err) {
+                console.error('Error retrieving expenses:', err.message);
+                callback(err, null);
+            } else {
+                callback(null, rows);
+            }
+        });
+    }
+
+    deleteExpense(id) {
+        const deleteQuery = `DELETE FROM expenses WHERE id = ?`;
+        this.database.run(deleteQuery, [id], function(err) {
+            if (err) {
+                console.error('Error deleting expense:', err.message);
+            } else if (this.changes === 0) {
+                console.log(`No expense found with ID: ${id}`);
+            } else {
+                console.log(`Expense deleted with ID: ${id}`);
+            }
+        });
     }
 
     calculateTotal() {
